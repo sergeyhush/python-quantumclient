7fbe2f236358a562407d58708d0031f694149359
sergeyhush <ssudakov@cisco.com>
2013-02-22 10:59:05 -0800

    simplified segment type

--- quantumclient/quantum/v2_0/networkprofile.py
+++ quantumclient/quantum/v2_0/networkprofile.py
@@ -16,9 +16,12 @@
 #@author Sergey Sudakovich, Cisco Systems
 
 import logging
-from quantumclient.quantum.v2_0 import ListCommand, ShowCommand, CreateCommand, DeleteCommand, UpdateCommand
+from quantumclient.quantum.v2_0 import ListCommand, ShowCommand, CreateCommand, DeleteCommand, UpdateCommand, \
+                                       QuantumCommand, parse_args_to_dict
+from quantumclient.common import exceptions
 
 RESOURCE = 'network_profile'
+SEGMENT_TYPE_CHOICES = ['vlan', 'vxlan']
 
 
 #TODO Finish parameters
@@ -48,28 +51,38 @@ class CreateNetworkProfile(CreateCommand):
     def add_known_arguments(self, parser):
         #TODO Change to mutually exclusive groups
         parser.add_argument('name', help='Name for Network Profile')
+        parser.add_argument('segment_type', choices=SEGMENT_TYPE_CHOICES, help='Segment type')
         parser.add_argument('--vlan', dest='vlan', action='store_true', help='VLAN')
         parser.add_argument('--segment_range', help='Range for the Segment')
         parser.add_argument('--vxlan', dest='vxlan', action='store_true', help='VxLAN')
         parser.add_argument('--multicast_ip_range', help='Multicast IPv4 Range')
         parser.add_argument("--add-tenant", help="Add tenant to the network profile")
-        parser.add_argument("--remove-tenant", help="Remove tenant from the network profile")
+
+        # parser.add_argument("--remove-tenant", help="Remove tenant from the network profile")
 
 
     def args2body(self, parsed_args):
         body = {'network_profile': {'name': parsed_args.name}}
-        if parsed_args.vlan:
-            body['network_profile'].update({'segment_type': 'vlan'})
-        if parsed_args.vxlan:
-            body['network_profile'].update({'segment_type': 'vxlan'})
+
+        # if parsed_args.vlan and parsed_args.vxlan:
+        #     raise exceptions.CommandError("--vlan option and "
+        #                                   "--vxlan option can "
+        #                                   "not be used same time")
+
+        # if parsed_args.vlan:
+        #     body['network_profile'].update({'segment_type': 'vlan'})
+        # if parsed_args.vxlan:
+        #     body['network_profile'].update({'segment_type': 'vxlan'})
+        if parsed_args.segment_type:
+            body['network_profile'].update({'segment_type': parsed_args.segment_type})
         if parsed_args.segment_range:
             body['network_profile'].update({'segment_range': parsed_args.segment_range})
         if parsed_args.multicast_ip_range:
             body['network_profile'].update({'multicast_ip_range': parsed_args.multicast_ip_range})
         if parsed_args.add_tenant:
             body['network_profile'].update({'add_tenant': parsed_args.add_tenant})
-        if parsed_args.remove_tenant:
-            body['network_profile'].update({'remove_tenant': parsed_args.remove_tenant})
+        # if parsed_args.remove_tenant:
+        #     body['network_profile'].update({'remove_tenant': parsed_args.remove_tenant})
         return body
 
 
@@ -86,3 +99,28 @@ class UpdateNetworkProfile(UpdateCommand):
 
     resource = RESOURCE
     log = logging.getLogger(__name__ + '.UpdateNetworkProfile')
+
+class UpdateNetworkProfileV2(QuantumCommand):
+
+    api = 'network'
+    log = logging.getLogger(__name__ + '.UpdateNetworkProfile')
+    resource = RESOURCE
+
+    def get_parser(self, prog_name):
+        parser = super(UpdateNetworkProfileV2, self).get_parser(prog_name)
+        parser.add_argument("--remove-tenant", help="Remove tenant from the network profile")
+        return parser
+
+    def run(self, parsed_args):
+        self.log.debug('run(%s)' % parsed_args)
+        quantum_client = self.get_client()
+        quantum_client.format = parsed_args.request_format
+        data = {self.resource: parse_args_to_dict(parsed_args)}
+        update_dict = {}
+        if parsed_args.remove_tenant:
+            data[self.resource]['remove_tenant'] = parsed_args.remove_tenant
+        quantum_client.update_network_profile(parsed_args.id, {self.resource: data})
+        print >>self.app.stdout, (
+                _('Updated %(resource)s: %(id)s') %
+                {'id': parsed_args.id, 'resource': self.resource})
+        return
